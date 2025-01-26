35310ed5be1ee41174a1faec68bcf830778d620f
sage-9 <olayiwolaladipo@gmail.com>
2025-01-26 19:49:13 +0100

    base character controller implementation
    started working on the player object and imported more assets

--- Assets/Script/Player3rdPersonMovement.cs
+++ Assets/Script/Player3rdPersonMovement.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class Player3rdPersonMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public CharacterController controller;
+    public float speed;
+    float horizontal;
+    float vertical;
 
+    
     // Update is called once per frame
     void Update()
     {
+        horizontal=Input.GetAxis("Horizontal");
+        vertical=Input.GetAxis("Vertical");
+        Vector3 direction= new Vector3 (horizontal,0,vertical);
+        controller.Move(direction*speed*Time.deltaTime);
         
     }
-}
+} 
