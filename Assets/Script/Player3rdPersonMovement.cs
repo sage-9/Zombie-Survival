3acbad037081cccd35c752b7dfdae07029c675b3
sage-9 <olayiwolaladipo@gmail.com>
2025-01-26 22:27:09 +0100

    updated Player movement script

--- Assets/Script/Player3rdPersonMovement.cs
+++ Assets/Script/Player3rdPersonMovement.cs
@@ -3,7 +3,12 @@ using UnityEngine;
 public class Player3rdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public Transform cam;
+
+
     public float speed;
+
+    
     float horizontal;
     float vertical;
 
@@ -11,10 +16,19 @@ public class Player3rdPersonMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 rotateDirection = new Vector3(0,cam.eulerAngles.y,0);
+        Quaternion rotateAngles= Quaternion.Euler(rotateDirection);        
         horizontal=Input.GetAxis("Horizontal");
         vertical=Input.GetAxis("Vertical");
-        Vector3 direction= new Vector3 (horizontal,0,vertical);
-        controller.Move(direction*speed*Time.deltaTime);
+        Vector3 direction= new Vector3 (horizontal,0,vertical).normalized;
+        direction=transform.TransformDirection(direction);
+        
+        if(direction.magnitude>0.01)
+        {
+            transform.localRotation = rotateAngles;
+            controller.Move(direction*speed*Time.deltaTime);
+        }
+        
         
     }
 } 
