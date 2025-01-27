494564a5319902805453157beefb5830042f1d8b
sage-9 <olayiwolaladipo@gmail.com>
2025-01-27 22:59:29 +0100

    Character movement completed

--- Assets/Script/Player3rdPersonMovement.cs
+++ Assets/Script/Player3rdPersonMovement.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Player3rdPersonMovement : MonoBehaviour
@@ -5,31 +6,28 @@ public class Player3rdPersonMovement : MonoBehaviour
     public CharacterController controller;
     public Animator anim;
     public Transform cam;
-
-
-    public float speed;
-
-    
+    public float speed;    
     float horizontal;
     float vertical;
+    float turnSmoothVelocity;
+    float turnSmoothTime=0.1f;
 
     
     // Update is called once per frame
     void Update()
     {
         anim.SetBool("IsWalking",false);
-        Vector3 rotateDirection = new Vector3(0,cam.eulerAngles.y,0);
-        Quaternion rotateAngles= Quaternion.Euler(rotateDirection);        
-        horizontal=Input.GetAxis("Horizontal");
-        vertical=Input.GetAxis("Vertical");
-        Vector3 direction= new Vector3 (horizontal,0,vertical).normalized;
-        direction=transform.TransformDirection(direction);
-        
+        horizontal=Input.GetAxisRaw("Horizontal");
+        vertical=Input.GetAxisRaw("Vertical");
+        Vector3 direction= new Vector3 (horizontal,0,vertical).normalized;       
         if(direction.magnitude>0.01)
         {
-            anim.SetBool("IsWalking",true);
-            transform.localRotation = rotateAngles;
-            controller.Move(direction*speed*Time.deltaTime);
+            float targetAngle = Mathf.Atan2(direction.x,direction.z)*Mathf.Rad2Deg+cam.eulerAngles.y;
+            float angle= Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle,ref turnSmoothVelocity,turnSmoothTime);
+            transform.rotation=Quaternion.Euler(0,angle,0);
+            Vector3 movDir=Quaternion.Euler(0,targetAngle,0)*Vector3.forward;                      
+            controller.Move(movDir.normalized*speed*Time.deltaTime);
+            anim.SetBool("IsWalking",true);  
         }
         
         
