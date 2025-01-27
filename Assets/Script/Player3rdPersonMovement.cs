928dc0c4be3db33445f89d7b40c228b6d416fe06
sage-9 <olayiwolaladipo@gmail.com>
2025-01-28 00:01:03 +0100

    Character Run animation setup
    added running functionality to the character and cleaned up the folder and file arrangement

--- Assets/Script/Player3rdPersonMovement.cs
+++ Assets/Script/Player3rdPersonMovement.cs
@@ -1,5 +1,6 @@
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player3rdPersonMovement : MonoBehaviour
 {
@@ -15,20 +16,21 @@ public class Player3rdPersonMovement : MonoBehaviour
     
     // Update is called once per frame
     void Update()
-    {
-        anim.SetBool("IsWalking",false);
+    {        
         horizontal=Input.GetAxisRaw("Horizontal");
         vertical=Input.GetAxisRaw("Vertical");
-        Vector3 direction= new Vector3 (horizontal,0,vertical).normalized;       
+        Vector3 direction= new Vector3 (horizontal,0,vertical).normalized;
+
         if(direction.magnitude>0.01)
         {
             float targetAngle = Mathf.Atan2(direction.x,direction.z)*Mathf.Rad2Deg+cam.eulerAngles.y;
             float angle= Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle,ref turnSmoothVelocity,turnSmoothTime);
             transform.rotation=Quaternion.Euler(0,angle,0);
             Vector3 movDir=Quaternion.Euler(0,targetAngle,0)*Vector3.forward;                      
-            controller.Move(movDir.normalized*speed*Time.deltaTime);
-            anim.SetBool("IsWalking",true);  
+            controller.Move(movDir.normalized*speed*Time.deltaTime);            
         }
+        
+        anim.SetFloat("speed",direction.magnitude*speed);
         
         
     }
